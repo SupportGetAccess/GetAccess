6105af4bb006dc08e1d657ad07717208ec94e8a9
Guillermo <guilles10@yahoo.com.ar>
2026-05-07 18:31:24 -0300

    Fix(api): usar EXTERNALPOSGA001 como external_pos_id

--- backend/main.py
+++ backend/main.py
@@ -77,7 +77,7 @@ except ImportError:
     BREVO_API_URL = "https://api.brevo.com/v3/smtp/email"
     MERCADOPAGO_API_URL = "https://api.mercadopago.com"
     MERCADOPAGO_ACCESS_TOKEN = "APP_USR-2888302331727804-031609-eb4c51fc6c1654d701d4a5f3b24fbcd7-1921694"
-    MERCADOPAGO_QR_EXTERNAL_POS_ID = "GetAccess QR"
+    MERCADOPAGO_QR_EXTERNAL_POS_ID = "EXTERNALPOSGA001"
     QUICKCHART_URL = "https://quickchart.io/qr"
     EMAIL_LOGO_URL = "https://getaccess.now.sh/logo.png"
     DEFAULT_EVENT_IMAGES = [
